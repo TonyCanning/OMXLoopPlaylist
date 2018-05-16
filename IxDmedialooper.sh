a2a84bb5deeba9b18279af6a0cd7f631eab8c5a8
TonyCanning <TonyCanning@users.noreply.github.com>
2018-05-16 23:49:59 +0100

    Update IxDmedialooper.sh

--- IxDmedialooper.sh
+++ IxDmedialooper.sh
@@ -32,7 +32,7 @@ mkdir IxD
 cd IxD
 
 # Creating a script which will run the looping playlist in OMXPlayer
-cat <<EOF > loopplaylist.sh
+cat <<'EOF' > loopplaylist.sh
 #!/bin/sh
 
 trap exit 1 SIGINT SIGKILL SIGTERM
@@ -59,7 +59,7 @@ SIGINT
 
 fi
 done
-EOF
+'EOF'
 
 # changing the file to EXECUTABLE
 
